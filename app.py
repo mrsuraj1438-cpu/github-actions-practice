ec5700fe47f12744bd096f8fad916d55c054723b
Suraj <mrsuraj1438@gmail.com>
2026-03-14 21:59:05 +0530

    fix the app.run line in python

--- app.py
+++ app.py
@@ -13,9 +13,5 @@ def health():
 
 
 if __name__ == "__main__":
-
-
-# Run Flask accessible from outside the container
-
-
-  app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
+    
